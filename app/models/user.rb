da795d89872f8f97557460e7d9135b3dedb1938d
icelydone <soren03@hotmail.com>
2019-05-06 12:15:22 -0400

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, :password, presence: true,
+  validates :username, uniqueness: true
+
   belongs_to :room
   has_many :comments
 end
