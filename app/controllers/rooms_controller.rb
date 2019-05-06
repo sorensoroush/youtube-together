a3a3b3e21eb547eef211b99ccfdd0780c01a360e
icelydone <soren03@hotmail.com>
2019-05-06 15:47:25 -0400

    Add authenication/authorization logic

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,5 @@
 class RoomsController < ApplicationController
+  before_action :authorize_request
   before_action :set_room, only: [:show, :update, :destroy]
 
   # GET /rooms
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/register', to: 'authentication#register'
+  post '/login', to: 'authentication#login'
   resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
